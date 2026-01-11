1c02b0c4b59db8739f022dc5d50662458881ebb6
boss <bartellbe@gmail.com>
2026-01-11 16:44:50 +0000

    Add basic gendiff command and lib export

---
+++ bin/gendiff.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+import genDiff from '../src/gendiff.js'
+import { createRequire } from 'node:module'
+
+
+const require = createRequire(import.meta.url)
+const { version } = require('../package.json')
+
+const program = new Command()
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .version(version, '-V, --version', 'output the version number')
+
+program.parse()
+
+const [filepath1, filepath2] = program.args
+
+const diff = genDiff(filepath1, filepath2)
+console.log(diff)
+
---
+++ src/gendiff.js
@@ -0,0 +1,6 @@
+import { Command } from 'commander'
+
+
+export default function genDiff(filepath1, filepath2) {
+  return `diff between ${filepath1} and ${filepath2}`
+}
